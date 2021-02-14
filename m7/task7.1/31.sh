6cb9ab83c0cf0ad56d900e3c254b04bb6fe33829
Oleksii Volchenko <alexey.dnepr.ua2@gmail.com>
2021-02-15 00:12:16 +0200

    Task 7.1 Block C Debuging Inode Decoder

--- m7/task7.1/31.sh
+++ m7/task7.1/31.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 echo -e "\nSOURCE ==>" "$1" "DESTINATION ===>" "$2" | logger
-ONE=$1 TWO=$2
-inodeElement=$(stat -c %i $ONE)
-input=$(find -inum $inodeElement|cut -b 2-255)
-#max length file in linux is 255 english symbols or "127,5" cyrillic symbols
 
-inputBranch=$(pwd $ONE)
-outputDestination=$(echo "$TWO""$input")
+echo -e "\nSOURCE ==>" "$1" "DESTINATION ===>" "$2"
+inodeElement=$(stat -c %i "$1")
+echo "InodeEl:" "$inodeElement"
+input=$(find . -inum "$inodeElement" | cut -b 2-255)
+#max length file in linux is 255 english symbols or "127,5" cyrillic symbols
+#3409341
+inputBranch=$(pwd $1)
+outputDestination=$(echo "$2""$input")
 echo "Input:" "$input"
 echo "InputBranch:" "$inputBranch"
 echo "Output Result:" "$outputDestination"
 input=0
-input=$(rsync -apzhtb --info=BACKUP2,COPY1,DEL1,REMOVE2,SKIP2,NAME1 --dry-run $ONE $TWO)
+input=$(rsync -apzhtb --info=BACKUP2,COPY1,DEL1,REMOVE2,SKIP2,NAME1 --dry-run $1 $2)
 destination=$(rsync -apzhtb --info=BACKUP2,COPY1,DEL1,REMOVE2,SKIP2,NAME1 --dry-run $outputDestination $inputBranch)
 
 echo "in" $input
@@ -22,7 +24,7 @@ echo "dest" $destination
 if [[ -n "$input" ]];
      then
         echo $input | sed 's/ /\n/g' | sed 's/$/ *NEW/' | logger
-        rsync -apzhtb $ONE $TWO
+        rsync -apzhtb $1 $2
 fi
 
 if [[ -n "$destination" ]];
@@ -30,11 +32,12 @@ if [[ -n "$destination" ]];
         echo $destination | sed 's/ /\n/g' | sed 's/$/ !DELETE on Destination/' | logger 
 fi
 
-input=$(crontab -l -u $USER | grep "./31.sh")
+input=$(crontab -l -u $USER | grep "/tmp/backup_folders.sh")
 #if [[ $input!=FALSE ]];
 if [[ -n input ]];
      then
-        sudo echo "5 /1 * * * *   main    ./31.sh" "$ONE" "$TWO" >> /var/spool/cron/crontabs/$USER
+        cp ./31.sh  /tmp/backup_folders.sh
+        sudo echo "5 /1 * * * *   main    /tmp/backup_folders.sh" "$1" "$2" >> /var/spool/cron/crontabs/$USER
         sudo systemctl restart cron.service
 fi
 #sudo cat /var/log/syslog | grep "*NEW"
