4cf48ab952c6d7510c022337835fdc67444d449b
Oleksii Volchenko <alexey.dnepr.ua2@gmail.com>
2021-02-15 01:42:13 +0200

    Task 7.1 Block C Complete

--- m7/task7.1/31.sh
+++ m7/task7.1/31.sh
@@ -2,23 +2,28 @@
 
 echo -e "\nSOURCE ==>" "$1" "DESTINATION ===>" "$2" | logger
 
-echo -e "\nSOURCE ==>" "$1" "DESTINATION ===>" "$2"
-inodeElement=$(stat -c %i "$1")
-echo "InodeEl:" "$inodeElement"
-input=$(find . -inum "$inodeElement" | cut -b 2-255)
+#echo -e "\nSOURCE ==>" "$1" "DESTINATION ===>" "$2"
+#inodeElement=$(stat -c %i "$1")
+#echo "InodeEl:" "$inodeElement"
+#echo "1" "$1"
+#specific w
+#input=$(find . -inum "$inodeElement" | cut -b 2-255)
 #max length file in linux is 255 english symbols or "127,5" cyrillic symbols
-#3409341
-inputBranch=$(pwd $1)
-outputDestination=$(echo "$2""$input")
-echo "Input:" "$input"
-echo "InputBranch:" "$inputBranch"
-echo "Output Result:" "$outputDestination"
+#inputBranch=$(pwd $1)
+#метод старый и дубовый конечно, но пусть а то зае
+
+input=$(stat -c %n "$1"|rev| cut -d "/" -f 1|rev)
+inputBranch=$(stat -c %n "$1" |rev| cut --complement -d "/" -f 1|rev)
+outputDestination=$(echo "$2""/""$input")
+#echo "Input:" "$input"
+#echo "InputBranch:" "$inputBranch"
+#echo "Output Result:" "$outputDestination"
 input=0
 input=$(rsync -apzhtb --info=BACKUP2,COPY1,DEL1,REMOVE2,SKIP2,NAME1 --dry-run $1 $2)
 destination=$(rsync -apzhtb --info=BACKUP2,COPY1,DEL1,REMOVE2,SKIP2,NAME1 --dry-run $outputDestination $inputBranch)
 
-echo "in" $input
-echo "dest" $destination
+#echo "in" $input
+#echo "dest" $destination
 
 
 if [[ -n "$input" ]];
